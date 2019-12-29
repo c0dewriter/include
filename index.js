915f95e019209c4ba5e73c80a2bf529f1ed5ea03
c0dewriter <mat_shaian@yahoo.com>
2019-12-29 17:25:39 +0330

    Minor changes + Commenting

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
+/**
+	While being fully aware that using <require()> inside functions is not a good practice,
+	I Still decided do it as I was worried the synchronous nature of require, would cause 
+	performance issues in the long run.
+*/
 function include(lib) 
 {
 	try { 
-		return require( require('./lib/includemap').resolve(lib) );}
+		return require( require('./lib/includemap').interpret(lib) ) }
 
 	catch (e) { 
 		require('./lib/error-handler').handle(e) }
 }
-
-
-include("$_LIBROOT/common/printer").print();
-
 
 module.exports = include;
