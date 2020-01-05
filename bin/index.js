adbfc082421d053d8831f96575f6300920bfdc4a
c0dewriter <mat_shaian@yahoo.com>
2020-01-05 16:49:54 +0330

    CLI interactive .includemap creation & Some moving around (again)

--- bin/index.js
+++ bin/index.js
@@ -1,29 +1,45 @@
 #!/usr/bin/env node
 
 try {
+	require('../lib/core/env').setIncludeMap();
 
 	require('yargs')
+		.strict()
 		.scriptName('include')
 		.parserConfiguration({ 'boolean-negation': false })
+		.demandCommand(1, 'No commands specified.')
 		.usage('Usage: $0 <command> [options?] ')
 
 		.command({
 			command: 'check',
 			describe: 'Check .includemap\'s existence and readability',
 			handler: () => {
-				require('../lib/core/env').setIncludeMap();
-				
-				require('./cli/check.js').check();
+				require('../lib/cli/check.js').check();
 			}
 		})
-		// .command({
-		// 	command: 'map',
-		// 	handler: () => {
-		// 		require('./cli/mapper.js').map();
-		// 	}
-		// })
+
+		.command({
+			command: 'map',
+			describe: 'Generate an include map based on a library folder',
+			// builder: {
+			// 	'l': {
+			// 		alias: 'limit-to',
+			// 		demandOption: false,
+			// 		choices: ['folders', 'files', 'nothing'],
+			// 		describe: 'Limit what things to map',
+			// 		default: 'nothing',
+			// 		type: 'string'
+			// 	}
+			// },
+
+			handler: () => require('../lib/cli/mapper.js').map()
+		})
+
+		
+		.showHelpOnFail(false, 'Use --help for available commands and their options')
 		.help('help').alias('h', 'help')
-		.version()
+		.version().alias('v', 'version')
+		// .epilogue('For more information, visit: https://github.com/c0dewriter/include-nodejs#readme')
 		.argv;
 
 } catch (e) { require('../lib/core/error-handler').handle(e);  }
